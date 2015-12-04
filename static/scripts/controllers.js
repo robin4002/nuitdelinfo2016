d97e2cd4c2f168d61d1d8003e3a5fc30739eaa61
Nino <nino.saccomani@outlook.com>
2015-12-04 05:59:01 +0100

    Nothing changed

--- static/scripts/controllers.js
+++ static/scripts/controllers.js
@@ -2,9 +2,7 @@
 	var controllers = angular.module('controllers', []);
 	
 	controllers.controller('MapController', ['$scope', function($scope) {
-		$scope.$root.safeState = "start";
 		
-<<<<<<< HEAD
 	}])
 	.controller('InfosController', ['$scope', function($scope) {
 		
@@ -13,23 +11,13 @@
 			catastropheType : "avalanche" 
 		}
 		
-=======
-		$scope.saveMe = function(){
-			$scope.$root.safeState = "unsafe";
+	}])
+	.controller('notifsController', ['$scope', function($scope) {
+		
+		$scope.infos = {
+			hide : true,
+			catastropheType : "avalanche" 
 		}
 		
-		$scope.signalMeSafe = function(){
-			$scope.$root.safeState = "safe";
-		}
-	}]);
-	
-	controllers.controller('TestController', ['$scope', 'ModelsManager', function($scope, ModelsManager) {
-		$scope.SafePlacesManager = new ModelsManager('safePlace');
-		$scope.SafePlacesManager.get().then(function(safePlaces) {
-			console.log(safePlaces);
-		}, function(data) {
-			console.log(data);
-		});
->>>>>>> eeaceafa98df6204efea007356871ddd039c2bd8
 	}]);
 }).call(this);
