ed4cbf0615b232522d1a352a668adf6f1697e6b7
Charles-Antoine CATALDI <Charles-Antoine CATALDI>
2015-12-04 05:30:09 +0100

    Change things

--- static/scripts/controllers.js
+++ static/scripts/controllers.js
@@ -1,8 +1,97 @@
 (function() {
 	var controllers = angular.module('controllers', []);
 	
-	controllers.controller('MapController', ['$scope', function($scope) {
+	controllers.controller('MapController', ['$scope', '$timeout', 'ModelsManager', function($scope, $timeout, ModelsManager) {
+		$scope.SafePlacesManager = new ModelsManager('safePlace');
+		$scope.DangerousPlacesManager = new ModelsManager('dangerousPlace');
+		$scope.HealthPlacesManager = new ModelsManager('healthPlace');
+		$scope.map;
+		$scope.markers = new Array();
+		$scope.directionsService = new google.maps.DirectionsService;
+		$scope.directionsDisplay = new google.maps.DirectionsRenderer;
 		
+		$scope.findPathToward = function(destination) {
+			
+		}
+		
+		$timeout(function() {
+			var initMap = function() {
+				$scope.map = new google.maps.Map(document.getElementById('map'), {
+					center: {
+						lat: -34.397,
+						lng: 150.644
+					},
+					zoom: 17
+				});
+				$scope.directionsDisplay.setMap($scope.map);
+				
+				navigator.geolocation.watchPosition(function(position) {
+					$scope.userPosition = {
+						lat: position.coords.latitude,
+						lng: position.coords.longitude
+					};
+					$scope.map.setCenter(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+				});
+				
+				$scope.SafePlacesManager.get().then(function(safePlaces) {
+					$scope.DangerousPlacesManager.get().then(function(dangerousPlaces) {
+						$scope.HealthPlacesManager.get().then(function(healthPlaces) {
+							for (var i in $scope.markers) {
+								$scope.markers[i].setMap($scope.map);
+							}
+							$scope.markers = new Array();
+							
+							for (var i in safePlaces) {
+								$scope.addMarker({
+									lat: safePlaces[i].coordinates[0],
+									lng: safePlaces[i].coordinates[1]
+								}, safePlaces[i].name, '/img/good.png');
+							}
+							
+							for (var i in dangerousPlaces) {
+								$scope.addMarker({
+									lat: dangerousPlaces[i].coordinates[0],
+									lng: dangerousPlaces[i].coordinates[1]
+								}, dangerousPlaces[i].name, '/img/bad.png');
+							}
+							
+							for (var i in healthPlaces) {
+								$scope.addMarker({
+									lat: healthPlaces[i].coordinates[0],
+									lng: healthPlaces[i].coordinates[1]
+								}, healthPlaces[i].name, '/img/good.png');
+							}
+						});
+					});
+				});
+			};
+			initMap();
+		});
+		
+		$scope.addMarker = function(coordinates, name, icon) {
+			var marker = new google.maps.Marker({
+				position: coordinates,
+				map: $scope.map,
+				title: name || '',
+				icon: icon
+			});
+			$scope.markers.push(marker);
+		};
+		
+		$scope.findPath = function(origin, destination) {
+			$scope.directionsService.route({
+				origin: origin,
+				destination: destination,
+				travelMode: google.maps.TravelMode.DRIVING
+			}, function(response, status) {
+				if (status === google.maps.DirectionsStatus.OK) {
+					console.log(response);
+					$scope.directionsDisplay.setDirections(response);
+				} else {
+					window.alert('Directions request failed due to ' + status);
+				}
+			});
+		};
 	}]);
 	
 	controllers.controller('TestController', ['$scope', 'ModelsManager', function($scope, ModelsManager) {
